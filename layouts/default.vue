fc354dd6b467f81bc935c174cec87c3f43003a4a
hamrayev <a.xamroyev@imkon.uz>
2024-06-05 18:45:23 +0500

    fix: build error

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <LayoutHeader class="sticky top-0 bg-white z-10" />
+    <!--    <LayoutHeader class="sticky top-0 bg-white z-10" />-->
     <div class="mt-4 pb-10">
       <slot />
     </div>
