da30fead6a7de6187663b013298113f35e2c920e
Sadulloh Fathullayev <s.fathullayev@uic.group>
2024-06-10 11:01:02 +0500

    fix: saved products

---
+++ pages/index/saved.vue
@@ -0,0 +1,14 @@
+<template>
+  <CommonSectionWrapper title="saved">
+    <template #header>
+      <div class="flex items-center">
+        <IconChevron class="text-gray" />
+        <p>{{ $t('go_back') }}</p>
+      </div>
+    </template>
+  </CommonSectionWrapper>
+</template>
+<script setup lang="ts">
+import IconChevron from '~/assets/icons/Common/chevron.svg'
+</script>
+<style scoped></style>
--- pages/saved.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <LayoutWrapper> </LayoutWrapper>
-  </div>
-</template>
-<script setup lang="ts"></script>
-<style scoped></style>
