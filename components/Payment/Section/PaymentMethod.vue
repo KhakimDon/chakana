d9441f3a3ec4aa74a0d0b42bcfdb535dcd76a113
OG'ABEK <o.shomurotov@uic.group>
2024-06-10 19:35:25 +0500

    fix: sidebar right side scrollables

---
+++ components/Payment/Section/PaymentMethod.vue
@@ -0,0 +1,19 @@
+<template>
+  <PaymentCardInfoHeader :title="$t('payment_method')">
+    <section class="space-y-2">
+      <PaymentCardInfo
+        icon="SvgoProfileMoney"
+        icon-class="text-green !text-2xl"
+        :title="$t('payment_method')"
+        @open-details="openModal = true"
+      />
+    </section>
+  </PaymentCardInfoHeader>
+  <ModalPaymentMethod v-model="openModal" />
+</template>
+
+<script setup lang="ts">
+const openModal = ref(false)
+</script>
+
+<style scoped></style>
