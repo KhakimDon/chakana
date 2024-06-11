48f1d8b3f1f9616914615c895da41ce454a60f49
Ibrohim Olimjonov <i.olimjonov@uic.group>
2024-06-11 09:24:12 +0500

    fix: addresses

--- pages/profile/addresses.vue
+++ pages/profile/addresses.vue
@@ -1,25 +1,43 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+const { list } = useListFetcher('/saved/address')
+
+const addAddressModal = ref(false)
+</script>
 
 <template>
-  <div
-    v-for="(item, index) in list"
-    :key="index"
-    :class="addressIdx === index ? '!border-orange' : ''"
-    class="bg-gray-300 p-3 rounded-xl w-full cursor-pointer group border border-transparent hover:border-orange transition-300"
-    @click="handleAddress(index)"
-  >
-    <div class="flex-center-between">
-      <img :src="item.icon" alt="" width="24px" height="24px" />
-      <div
-        class="bg-white rounded-full w-5 h-5 border-2 border-gray-200 transition-300"
-        :class="addressIdx === index ? 'border-orange border-[5px]' : ''"
-      ></div>
+  <div>
+    <div class="flex-center-between mb-4">
+      <h1 class="text-xl font-extrabold leading-7 text-dark">
+        {{ $t('addresses') }}
+      </h1>
+      <button
+        class="flex-y-center gap-1 text-red text-sm font-semibold leading-5 transition-300 group hover:text-orange"
+        @click="addAddressModal = true"
+      >
+        <SvgoCommonPlus
+          class="text-xl leading-5 text-red transition-300 group-hover:text-orange"
+        />
+        {{ $t('add_address') }}
+      </button>
     </div>
-    <h3 class="mt-3 text-dark font-bold">{{ item.address }}</h3>
-    <p class="line-clamp-1 text-xs text-gray-100 mt-1">
-      {{ item.street }}
-    </p>
+    <div class="grid grid-cols-4 gap-4">
+      <div
+        v-for="(item, index) in list"
+        :key="index"
+        class="bg-gray-300 p-3 rounded-xl w-full cursor-pointer group border border-transparent hover:border-orange transition-300 transition-300 group"
+      >
+        <div class="flex-center-between">
+          <img :src="item.icon" :alt="item.title" width="24px" height="24px" />
+          <SvgoCommonEditPenSquare
+            class="text-2xl leading-6 text-gray group-hover:text-orange transition-300"
+          />
+        </div>
+        <h3 class="mt-3 text-dark font-bold">{{ item.title }}</h3>
+        <p class="line-clamp-1 text-xs text-gray-100 mt-1">
+          {{ item.street }}
+        </p>
+      </div>
+    </div>
+    <CommonModalMap v-model="addAddressModal" />
   </div>
 </template>
-
-<style scoped></style>
