c517f44cff62358985bece8e49a6dd4915f30987
Sadulloh Fathullayev <s.fathullayev@uic.group>
2024-06-11 09:09:13 +0500

    fix: adress card

---
+++ components/Cart/Card/Adress.vue
@@ -0,0 +1,37 @@
+<template>
+  <div
+    v-for="(item, index) in list"
+    :key="index"
+    :class="addressIdx === index ? '!border-orange' : ''"
+    class="bg-gray-300 p-3 rounded-xl w-full cursor-pointer group border border-transparent hover:border-orange transition-300"
+    @click="handleAddress(index)"
+  >
+    <div class="flex-center-between">
+      <img :src="item.icon" alt="" width="24px" height="24px" />
+      <div
+        class="bg-white rounded-full w-5 h-5 border-2 border-gray-200 transition-300"
+        :class="addressIdx === index ? 'border-orange border-[5px]' : ''"
+      ></div>
+    </div>
+    <h3 class="mt-3 text-dark font-bold">{{ item.address }}</h3>
+    <p class="line-clamp-1 text-xs text-gray-100 mt-1">
+      {{ item.street }}
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  list: any[]
+}
+
+defineProps<Props>()
+
+const addressIdx = ref(0)
+
+const handleAddress = (index: number) => {
+  addressIdx.value = index
+}
+</script>
+
+<style scoped></style>
--- components/Common/Modal/AddressDelivery.vue
+++ components/Common/Modal/AddressDelivery.vue
@@ -8,25 +8,7 @@
   >
     <div v-if="list.length">
       <div class="flex items-center gap-4">
-        <div
-          v-for="(item, index) in list"
-          :key="index"
-          :class="addressIdx === index ? '!border-orange' : ''"
-          class="bg-gray-300 p-3 rounded-xl w-full cursor-pointer group border border-transparent hover:border-orange transition-300"
-          @click="handleAddress(index)"
-        >
-          <div class="flex-center-between">
-            <img :src="item.icon" alt="" width="24px" height="24px" />
-            <div
-              class="bg-white rounded-full w-5 h-5 border-2 border-gray-200 transition-300"
-              :class="addressIdx === index ? 'border-orange border-[5px]' : ''"
-            ></div>
-          </div>
-          <h3 class="mt-3 text-dark font-bold">{{ item.address }}</h3>
-          <p class="line-clamp-1 text-xs text-gray-100 mt-1">
-            {{ item.street }}
-          </p>
-        </div>
+        <CartCardAdress v-bind="{ list }" />
       </div>
       <BaseButton
         class="mt-6 w-full group"
@@ -41,7 +23,6 @@
           />
         </template>
       </BaseButton>
-      <!--    ToDo: Primary button ishlamaydi keyinroq ishlatib qo'yaman-->
       <BaseButton
         class="mt-4 w-full group"
         :loading="false"
