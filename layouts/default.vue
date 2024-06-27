e61e9c28c0973b9ece582eae97c5ab1ebcdf71ef
OG'ABEK <o.shomurotov@uic.group>
2024-06-27 15:31:30 +0500

    fix: navigation bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,7 +5,9 @@
       class="sticky top-0 bg-white z-10"
     />
     <LayoutHeaderMobile v-else class="sticky top-0 bg-white z-10" />
-    <div class="md:min-h-[calc(100vh-69px)] min-h-[calc(100vh-68px-64px)] py-4">
+    <div
+      class="md:min-h-[calc(100vh-69px)] min-h-[calc(100vh-68px-64px)] py-4 pb-10 md:pb-4"
+    >
       <slot />
     </div>
     <LayoutMobileBottomBar v-if="!useMobile('desktop')" />
