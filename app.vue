71b3cdad8c93b667e2f5d59089529c3dcbff2098
hamrayev <a.xamroyev@imkon.uz>
2024-08-17 17:21:27 +0500

    trying to fix seo

--- app.vue
+++ app.vue
@@ -1,20 +1,19 @@
 <template>
   <div>
-    xolodilnik
-    <!--    <NuxtLoadingIndicator-->
-    <!--      color="linear-gradient(183deg, #FF831B -51.95%, #F30 124.83%)"-->
-    <!--    />-->
-    <!--    <NuxtLayout>-->
-    <!--      <NuxtPage />-->
-    <!--    </NuxtLayout>-->
-    <!--    <LayoutAuthModal v-model="authStore.showAuth" />-->
-    <!--    <CommonLoading v-if="loading" />-->
+    <NuxtLoadingIndicator
+      color="linear-gradient(183deg, #FF831B -51.95%, #F30 124.83%)"
+    />
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+    <!--        <LayoutAuthModal v-model="authStore.showAuth" />-->
+    <!--        <CommonLoading v-if="loading" />-->
   </div>
 </template>
 <script setup lang="ts">
-import { load } from '@fingerprintjs/fingerprintjs'
-
-import { useAuthStore } from '~/store/auth'
+// import { load } from '@fingerprintjs/fingerprintjs'
+//
+// import { useAuthStore } from '~/store/auth'
 
 useSeoMeta({
   title: 'Холодильник Онлайн',
@@ -28,67 +27,67 @@ useSeoMeta({
   twitterImage: '/og-image.png',
 })
 
-const authStore = useAuthStore()
+// const authStore = useAuthStore()
+//
+// const loading = ref(true)
+//
+// const tokens = computed(() => authStore.getTokens())
+//
+// if (tokens.value?.access) {
+//   authStore.fetchUser()
+// }
+//
+// const fingerprint = useCookie('fingerprint')
+//
+// if (!fingerprint.value) {
+//   let fpPromise
+//
+//   if (process.client) {
+//     fpPromise = load()
+//   }
+//
+//   const fp = ref()
+//
+//   fpPromise
+//     .then((fp) => fp.get())
+//     .then((result) => (fp.value = result.visitorId))
+//     .then(() => {
+//       fingerprint.value = fp.value
+//     })
+//     .finally(() =>
+//       useApi()
+//         .$get('get/device')
+//         .then(() => {
+//           window.location.reload()
+//         })
+//     )
+// } else {
+//   loading.value = false
+// }
 
-const loading = ref(true)
+// useSeoMeta({
+//   title: 'Холодильник Онлайн',
+//   description:
+//     'Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!',
+//   ogTitle: `Холодильник Онлайн`,
+//   ogDescription: `Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!`,
+//   twitterTitle: `Холодильник Онлайн`,
+//   twitterCard: 'summary',
+//   twitterSite: '@xolodilnik',
+//   ogImage: '/og.png',
+//   twitterImage: '/og.png',
+// })
 
-const tokens = computed(() => authStore.getTokens())
-
-if (tokens.value?.access) {
-  authStore.fetchUser()
-}
-
-const fingerprint = useCookie('fingerprint')
-
-if (!fingerprint.value) {
-  let fpPromise
-
-  if (process.client) {
-    fpPromise = load()
-  }
-
-  const fp = ref()
-
-  fpPromise
-    .then((fp) => fp.get())
-    .then((result) => (fp.value = result.visitorId))
-    .then(() => {
-      fingerprint.value = fp.value
-    })
-    .finally(() =>
-      useApi()
-        .$get('get/device')
-        .then(() => {
-          window.location.reload()
-        })
-    )
-} else {
-  loading.value = false
-}
-
-useSeoMeta({
-  title: 'Холодильник Онлайн',
-  description:
-    'Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!',
-  ogTitle: `Холодильник Онлайн`,
-  ogDescription: `Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!`,
-  twitterTitle: `Холодильник Онлайн`,
-  twitterCard: 'summary',
-  twitterSite: '@xolodilnik',
-  ogImage: '/og.png',
-  twitterImage: '/og.png',
-})
-
-onMounted(() => {
-  if (useMobile('desktop')) {
-    useHead({
-      script: [
-        {
-          src: '//code.jivosite.com/widget/ri5Jw2oMql',
-          async: true,
-        },
-      ],
-    })
-  }
-})
+// onMounted(() => {
+//   if (useMobile('desktop')) {
+//     useHead({
+//       script: [
+//         {
+//           src: '//code.jivosite.com/widget/ri5Jw2oMql',
+//           async: true,
+//         },
+//       ],
+//     })
+//   }
+// })
 </script>
