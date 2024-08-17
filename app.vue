54e744aa9aa25a28a721318e3dd5c11c14ca39d3
hamrayev <a.xamroyev@imkon.uz>
2024-08-17 17:59:26 +0500

    fix: seo

--- app.vue
+++ app.vue
@@ -6,18 +6,20 @@
     <NuxtLayout>
       <NuxtPage />
     </NuxtLayout>
-    <!--        <LayoutAuthModal v-model="authStore.showAuth" />-->
-    <!--        <CommonLoading v-if="loading" />-->
+    <LayoutAuthModal v-model="authStore.showAuth" />
+    <CommonLoading v-if="loading" />
   </div>
 </template>
 <script setup lang="ts">
-// import { load } from '@fingerprintjs/fingerprintjs'
-//
-// import { useAuthStore } from '~/store/auth'
+import { load } from '@fingerprintjs/fingerprintjs'
+
+import { useAuthStore } from '~/store/auth'
 
 useSeoMeta({
   title: 'Холодильник Онлайн',
   description:
+    'Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!',
+  ogDescription:
     'Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!',
   ogTitle: 'Холодильник Онлайн',
   twitterTitle: 'Холодильник Онлайн',
@@ -27,67 +29,54 @@ useSeoMeta({
   twitterImage: '/og-image.png',
 })
 
-// const authStore = useAuthStore()
-//
-// const loading = ref(true)
-//
-// const tokens = computed(() => authStore.getTokens())
-//
-// if (tokens.value?.access) {
-//   authStore.fetchUser()
-// }
-//
-// const fingerprint = useCookie('fingerprint')
-//
-// if (!fingerprint.value) {
-//   let fpPromise
-//
-//   if (process.client) {
-//     fpPromise = load()
-//   }
-//
-//   const fp = ref()
-//
-//   fpPromise
-//     .then((fp) => fp.get())
-//     .then((result) => (fp.value = result.visitorId))
-//     .then(() => {
-//       fingerprint.value = fp.value
-//     })
-//     .finally(() =>
-//       useApi()
-//         .$get('get/device')
-//         .then(() => {
-//           window.location.reload()
-//         })
-//     )
-// } else {
-//   loading.value = false
-// }
+const authStore = useAuthStore()
 
-// useSeoMeta({
-//   title: 'Холодильник Онлайн',
-//   description:
-//     'Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!',
-//   ogTitle: `Холодильник Онлайн`,
-//   ogDescription: `Добро пожаловать в Магазин Холодильник — ваш универсальный онлайн-магазин для всех продуктов, требующих хранения в холодильнике! В нашем ассортименте вы найдете свежие и качественные молочные продукты, мясо, рыбу, овощи, фрукты, готовые блюда и многое другое. Мы обеспечиваем быструю и надежную доставку прямо к вашему порогу, чтобы вы могли наслаждаться вкусной и здоровой пищей каждый день. Откройте для себя удобство покупок с Магазин Холодильник!`,
-//   twitterTitle: `Холодильник Онлайн`,
-//   twitterCard: 'summary',
-//   twitterSite: '@xolodilnik',
-//   ogImage: '/og.png',
-//   twitterImage: '/og.png',
-// })
+const loading = ref(true)
 
-// onMounted(() => {
-//   if (useMobile('desktop')) {
-//     useHead({
-//       script: [
-//         {
-//           src: '//code.jivosite.com/widget/ri5Jw2oMql',
-//           async: true,
-//         },
-//       ],
-//     })
-//   }
-// })
+const tokens = computed(() => authStore.getTokens())
+
+if (tokens.value?.access) {
+  authStore.fetchUser()
+}
+
+const fingerprint = useCookie('fingerprint')
+
+if (!fingerprint.value) {
+  let fpPromise
+
+  if (process.client) {
+    fpPromise = load()
+  }
+
+  const fpd = ref()
+
+  fpPromise
+    ?.then((fp) => fp.get())
+    .then((result) => (fpd.value = result.visitorId))
+    .then(() => {
+      fingerprint.value = fpd.value
+    })
+    .finally(() =>
+      useApi()
+        .$get('get/device')
+        .then(() => {
+          window.location.reload()
+        })
+    )
+} else {
+  loading.value = false
+}
+
+onMounted(() => {
+  if (useMobile('desktop')) {
+    useHead({
+      script: [
+        {
+          src: '//code.jivosite.com/widget/ri5Jw2oMql',
+          async: true,
+        },
+      ],
+    })
+  }
+})
 </script>
