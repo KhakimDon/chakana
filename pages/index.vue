cb3a9df47b70b1b18d57e7f72d5147f846e217fb
Aydos <a.sankibaev@uic.group>
2024-08-19 14:48:19 +0500

    push notifications

--- pages/index.vue
+++ pages/index.vue
@@ -39,6 +39,9 @@
   </main>
 </template>
 <script setup lang="ts">
+import { getAuth, signInAnonymously } from 'firebase/auth'
+import { getMessaging, getToken, onMessage } from 'firebase/messaging'
+
 import { useCartStore } from '~/store/cart.js'
 import { useCategoriesStore } from '~/store/categories'
 
@@ -71,4 +74,51 @@ const changeCoords = () => {
 }
 
 function saveAddress(data: any) {}
+
+if (process.client) {
+  const messaging = getMessaging()
+
+  const activate = async () => {
+    const token = await getToken(messaging, {
+      vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
+    })
+
+    if (token) {
+      console.log('FCM token: ', token)
+
+      onMessage(messaging, (payload) => {
+        console.log('Message from firebase: ', payload)
+        return self?.registration?.showNotification(
+          payload.notification?.title,
+          {
+            body: payload.notification?.body,
+          }
+        )
+      })
+    } else {
+      console.log(
+        'No Instance ID token available. Request permission to generate one.'
+      )
+    }
+  }
+
+  const authFirebase = async () => {
+    try {
+      const { app } = await import('~/plugins/firebase.js')
+      const auth = getAuth(app)
+      await signInAnonymously(auth)
+      activate()
+      console.log('authFirebase successful')
+    } catch (error) {
+      console.error('authFirebase error:', error)
+      // Handle the error appropriately
+    }
+  }
+
+  onMessage(messaging, (payload) => {
+    console.log('client onMessage', payload)
+  })
+
+  authFirebase()
+}
 </script>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -4,24 +4,28 @@ import { getMessaging } from 'firebase/messaging'
 // CHANGE THIS TO YOUR APP ID
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDEv5OF3zxnwslBwba8-Lv7KuzJZtf8rkE',
-  authDomain: 'xolodilnik-55eb9.firebaseapp.com',
-  projectId: 'xolodilnik-55eb9',
-  storageBucket: 'xolodilnik-55eb9.appspot.com',
-  messagingSenderId: '667771902581',
-  appId: '1:667771902581:web:cb4db1e59ee2abf1bbf63c',
-  measurementId: 'G-YGJDLXQDWY',
+  apiKey: 'AIzaSyBhVDIWESPjDMIBNHwb8H_2DuladXbAvy0',
+  authDomain: 'xolodilnik-a9b1c.firebaseapp.com',
+  projectId: 'xolodilnik-a9b1c',
+  storageBucket: 'xolodilnik-a9b1c.appspot.com',
+  messagingSenderId: '982363687648',
+  appId: '1:982363687648:web:e81c44677fc8be41d35e85',
+  measurementId: 'G-XYCKJ6PDBY',
 }
 
 const apps = getApps()
 
-const app = apps.length > 0 ? apps[0] : initializeApp(firebaseConfig)
+export const app = apps.length > 0 ? apps[0] : initializeApp(firebaseConfig)
+
+export const messaging = getMessaging(app)
 
 export default defineNuxtPlugin(() => {
-  return {
-    provide: {
-      messaging: getMessaging(app),
-    },
+  if (process.client) {
+    return {
+      provide: {
+        messaging,
+      },
+    }
   }
 })
 
