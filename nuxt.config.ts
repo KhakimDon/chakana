0854eca72112d4621c339685947c676eeffe5fbb
hamrayev <a.xamroyev@imkon.uz>
2024-08-22 16:04:44 +0500

    fix: sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -115,33 +115,33 @@ export default defineNuxtConfig({
       },
     },
   },
-  sitemap: {
-    exclude: [
-      '/profile/**',
-      '/profile',
-      '/payment/**',
-      '/payment',
-      '/cart/**',
-      '/cart',
-    ],
-    xslColumns: [
-      { label: 'URL', width: '50%' },
-      { label: 'Last Modified', select: 'sitemap:lastmod', width: '25%' },
-      { label: 'Priority', select: 'sitemap:priority', width: '12.5%' },
-      {
-        label: 'Change Frequency',
-        select: 'sitemap:changefreq',
-        width: '12.5%',
-      },
-    ],
-    sitemaps: {
-      products: {
-        includeAppSources: false,
-
-        sources: ['/api/__sitemap__/urls/products'],
-      },
-    },
-  },
+  // sitemap: {
+  //   exclude: [
+  //     '/profile/**',
+  //     '/profile',
+  //     '/payment/**',
+  //     '/payment',
+  //     '/cart/**',
+  //     '/cart',
+  //   ],
+  //   xslColumns: [
+  //     { label: 'URL', width: '50%' },
+  //     { label: 'Last Modified', select: 'sitemap:lastmod', width: '25%' },
+  //     { label: 'Priority', select: 'sitemap:priority', width: '12.5%' },
+  //     {
+  //       label: 'Change Frequency',
+  //       select: 'sitemap:changefreq',
+  //       width: '12.5%',
+  //     },
+  //   ],
+  //   sitemaps: {
+  //     products: {
+  //       includeAppSources: false,
+  //
+  //       sources: ['/api/__sitemap__/urls/products'],
+  //     },
+  //   },
+  // },
 
   // plugins: [
   //   {
