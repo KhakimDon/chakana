e669136240ca7ed1e49af11a55470bf959b5b459
hamrayev <a.xamroyev@imkon.uz>
2024-09-13 19:06:09 +0500

    remove: firebase

--- pages/index.vue
+++ pages/index.vue
@@ -37,8 +37,8 @@
   </main>
 </template>
 <script setup lang="ts">
-import { getAuth, signInAnonymously } from 'firebase/auth'
-import { getMessaging, getToken, onMessage } from 'firebase/messaging'
+// import { getAuth, signInAnonymously } from 'firebase/auth'
+// import { getMessaging, getToken, onMessage } from 'firebase/messaging'
 
 import { useCartStore } from '~/store/cart.js'
 import { useCategoriesStore } from '~/store/categories'
@@ -73,47 +73,47 @@ const changeCoords = () => {
 
 function saveAddress(data: any) {}
 
-if (process.client) {
-  const messaging = getMessaging()
-
-  const activate = async () => {
-    const token = await getToken(messaging, {
-      vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
-    })
-
-    if (token) {
-      onMessage(messaging, (payload) => {
-        console.log('Message from firebase: ', payload)
-        return self?.registration?.showNotification(
-          payload.notification?.title,
-          {
-            body: payload.notification?.body,
-          }
-        )
-      })
-    } else {
-      console.log(
-        'No Instance ID token available. Request permission to generate one.'
-      )
-    }
-  }
-
-  const authFirebase = async () => {
-    try {
-      const { app } = await import('~/plugins/firebase.js')
-      const auth = getAuth(app)
-      await signInAnonymously(auth)
-      activate()
-      console.log('authFirebase successful')
-    } catch (error) {
-      console.error('authFirebase error:', error)
-    }
-  }
-
-  onMessage(messaging, (payload) => {
-    console.log('client onMessage', payload)
-  })
-
-  authFirebase()
-}
+// if (process.client) {
+//   const messaging = getMessaging()
+//
+//   const activate = async () => {
+//     const token = await getToken(messaging, {
+//       vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
+//     })
+//
+//     if (token) {
+//       onMessage(messaging, (payload) => {
+//         console.log('Message from firebase: ', payload)
+//         return self?.registration?.showNotification(
+//           payload.notification?.title,
+//           {
+//             body: payload.notification?.body,
+//           }
+//         )
+//       })
+//     } else {
+//       console.log(
+//         'No Instance ID token available. Request permission to generate one.'
+//       )
+//     }
+//   }
+//
+//   const authFirebase = async () => {
+//     try {
+//       const { app } = await import('~/plugins/firebase.js')
+//       const auth = getAuth(app)
+//       await signInAnonymously(auth)
+//       activate()
+//       console.log('authFirebase successful')
+//     } catch (error) {
+//       console.error('authFirebase error:', error)
+//     }
+//   }
+//
+//   onMessage(messaging, (payload) => {
+//     console.log('client onMessage', payload)
+//   })
+//
+//   authFirebase()
+// }
 </script>
--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,30 +1,30 @@
-import { getApps, initializeApp } from 'firebase/app'
-import { getMessaging, isSupported } from 'firebase/messaging'
-
-const firebaseConfig = {
-  apiKey: 'AIzaSyBhVDIWESPjDMIBNHwb8H_2DuladXbAvy0',
-  authDomain: 'xolodilnik-a9b1c.firebaseapp.com',
-  projectId: 'xolodilnik-a9b1c',
-  storageBucket: 'xolodilnik-a9b1c.appspot.com',
-  messagingSenderId: '982363687648',
-  appId: '1:982363687648:web:e81c44677fc8be41d35e85',
-  measurementId: 'G-XYCKJ6PDBY',
-}
-
-const apps = getApps()
-export const app = apps.length > 0 ? apps[0] : initializeApp(firebaseConfig)
-
-export default defineNuxtPlugin(async (nuxtApp) => {
-  const { $isClient } = useNuxtApp()
-
-  if ($isClient) {
-    // Check if messaging is supported in the current browser
-    const supported = await isSupported()
-    if (supported) {
-      const messaging = getMessaging(app)
-      nuxtApp.provide('messaging', messaging)
-    } else {
-      console.warn('Firebase Messaging is not supported in this browser.')
-    }
-  }
-})
+// import { getApps, initializeApp } from 'firebase/app'
+// import { getMessaging, isSupported } from 'firebase/messaging'
+//
+// const firebaseConfig = {
+//   apiKey: 'AIzaSyBhVDIWESPjDMIBNHwb8H_2DuladXbAvy0',
+//   authDomain: 'xolodilnik-a9b1c.firebaseapp.com',
+//   projectId: 'xolodilnik-a9b1c',
+//   storageBucket: 'xolodilnik-a9b1c.appspot.com',
+//   messagingSenderId: '982363687648',
+//   appId: '1:982363687648:web:e81c44677fc8be41d35e85',
+//   measurementId: 'G-XYCKJ6PDBY',
+// }
+//
+// const apps = getApps()
+// export const app = apps.length > 0 ? apps[0] : initializeApp(firebaseConfig)
+//
+// export default defineNuxtPlugin(async (nuxtApp) => {
+//   const { $isClient } = useNuxtApp()
+//
+//   if ($isClient) {
+//     // Check if messaging is supported in the current browser
+//     const supported = await isSupported()
+//     if (supported) {
+//       const messaging = getMessaging(app)
+//       nuxtApp.provide('messaging', messaging)
+//     } else {
+//       console.warn('Firebase Messaging is not supported in this browser.')
+//     }
+//   }
+// })
