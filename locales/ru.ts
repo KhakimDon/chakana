86b273f141cc4652bbbabeead4f860834602677e
hamrayev <a.xamroyev@imkon.uz>
2024-09-13 15:25:44 +0500

    fix: translations

--- locales/ru.ts
+++ locales/ru.ts
@@ -2,7 +2,7 @@ const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 export default async () => {
   const fingerprint = useCookie('fingerprint')
-  const data = await fetch(`${BASE_URL}/translations/`, {
+  const data = await fetch(`${BASE_URL}/translations`, {
     headers: {
       'Accept-Language': 'ru',
       'Device-Id': fingerprint.value,
